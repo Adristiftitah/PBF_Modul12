703b2bdb386525045baee57ce29b2f99c900f966
Adristiftitah <adristiiftitahyuniar@gmail.com>
2021-04-28 18:44:23 +0700

    praktikum modul12

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7RMuRx7wonX8nznLy_mpR6D44uDXd5BA",
+    authDomain: "reactjs-31762.firebaseapp.com",
+    databaseURL: "https://reactjs-31762-default-rtdb.firebaseio.com",
+    projectId: "reactjs-31762",
+    storageBucket: "reactjs-31762.appspot.com",
+    messagingSenderId: "192032626579",
+    appId: "1:192032626579:web:698aa8854ccb1686947a66",
+    measurementId: "G-QV4KD8SGFH",
+  };
+  
+  export default firebaseConfig;
+  
